1234c777da3de2162291e048e03de61c6dcce428
abunaum <abunaum@hotmail.com>
2020-08-30 12:51:17 +0700

    redirect setelah connect router

--- app/Controllers/Router/Beranda.php
+++ app/Controllers/Router/Beranda.php
@@ -8,6 +8,15 @@ class Beranda extends BaseController
 {
     public function index()
     {
-        echo 'joss';
+        $data = [
+            'title' => 'Dashboard',
+            'namaaplikasi' => $this->namaaplikasi,
+        ];
+        // dd($totalrouter);
+        echo view('template/app/header', $data);
+        echo view('template/app/sidebaradmin', $data);
+        echo view('template/app/topbar', $data);
+        echo view('router/index', $data);
+        echo view('template/app/footer', $data);
     }
 }
